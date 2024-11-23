2cd92d7c606f7b58f4fc0fd033d92a1737e6a776
domson <qeeu.que@gmail.com>
2024-11-23 19:23:07 +0100

    pieces

--- src/main/Main.java
+++ src/main/Main.java
@@ -31,10 +31,33 @@ public class Main extends Application
 
 		//SZACHOWNICA BOARD.java
 		Board chessboard = new Board();
-		chessboard.addPiece(0, 1, Type.PAWN, PieceColor.WHITE); // Biały pionek
-        chessboard.addPiece(0, 6, Type.PAWN, PieceColor.BLACK); // Czarny pionek
-        chessboard.addPiece(4, 0, Type.QUEEN, PieceColor.WHITE); // Biała królowa
-        chessboard.addPiece(4, 7, Type.KING, PieceColor.BLACK); // Czarny król
+
+		//Black pieces
+        chessboard.addPiece(0, 0, Type.ROOK, PieceColor.BLACK);
+		chessboard.addPiece(1, 0, Type.KNIGHT, PieceColor.BLACK);
+		chessboard.addPiece(2, 0, Type.BISHOP, PieceColor.BLACK);
+		chessboard.addPiece(3, 0, Type.QUEEN, PieceColor.BLACK);
+		chessboard.addPiece(4, 0, Type.KING, PieceColor.BLACK);
+		chessboard.addPiece(5, 0, Type.BISHOP, PieceColor.BLACK);
+		chessboard.addPiece(6, 0, Type.KNIGHT, PieceColor.BLACK);
+		chessboard.addPiece(7, 0, Type.ROOK, PieceColor.BLACK);
+
+		for(int i = 0; i < 8; i++)
+			chessboard.addPiece(i, 1, Type.PAWN, PieceColor.BLACK);
+
+	
+		// White pieces
+		chessboard.addPiece(0, 7, Type.ROOK, PieceColor.WHITE);
+		chessboard.addPiece(1, 7, Type.KNIGHT, PieceColor.WHITE);
+		chessboard.addPiece(2, 7, Type.BISHOP, PieceColor.WHITE);
+		chessboard.addPiece(3, 7, Type.QUEEN, PieceColor.WHITE);
+		chessboard.addPiece(4, 7, Type.KING, PieceColor.WHITE);
+		chessboard.addPiece(5, 7, Type.BISHOP, PieceColor.WHITE);
+		chessboard.addPiece(6, 7, Type.KNIGHT, PieceColor.WHITE);
+		chessboard.addPiece(7, 7, Type.ROOK, PieceColor.WHITE);
+
+		for(int i = 0; i < 8; i++)
+			chessboard.addPiece(i, 6, Type.PAWN, PieceColor.WHITE);
 
 
 		chessboard.setPrefSize(640, 640);
